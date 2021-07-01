77ed901722fc37dcaa14913b131b4d7b0683a15e
john million <jpmillion@comcast.net>
2021-06-30 20:08:13 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+    newnum = num 
+    largest_prime = 0
+    possible_factor = 2
+    
+    while possible_factor**2 <= newnum
+        if newnum % possible_factor == 0
+            newnum /= possible_factor
+            largest_prime = possible_factor
+        else
+            possible_factor += 1
+        end
+    end
+    
+    largest_prime = newnum unless largest_prime >= newnum
+    largest_prime
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_accessor :input, :largest_prime
+
+    def initialize(input)
+        @input = input
+    end
+
+    def number
+        newnum = input
+        possible_factor = 2
+
+        while possible_factor**2 <= newnum
+            if newnum % possible_factor == 0
+                newnum /= possible_factor
+                largest_prime = possible_factor
+            else
+                possible_factor += 1
+            end
+        end
+        largest_prime = newnum unless largest_prime >= newnum
+        largest_prime
+    end
+end
